fc8c9d997ddb63434af2a4c64f8f12567eb9a69d
Ayushi2104 <patel.2004aayushi@gmail.com>
2022-10-29 10:17:32 -0400

    First Commit

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports =
+ {
+ //"URI":"mongodb://127.0.0.1:27017/book_store"
+ "URI": "mongodb+srv://Ayushi:TgEBqD6zn6jWPZQb@clustert.ph9ittl.mongodb.net/contact_info?retryWrites=true&w=majority",
+ "Secret":"SomeSecret"
+}
+//TgEBqD6zn6jWPZQb
